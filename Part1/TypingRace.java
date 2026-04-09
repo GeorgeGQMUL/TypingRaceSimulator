f0fcd40786fa92c642258b9c437f0dfa73f4452a
TheGregLloyd <george.guest@outlook.com>
2026-04-10 00:28:54 +0100

    added the main method in order to run and test the program

--- Part1/TypingRace.java
+++ Part1/TypingRace.java
@@ -49,6 +49,14 @@ public class TypingRace
         seat3Typist = null;
     }
 
+    public static void main(String[] args) {
+    TypingRace race = new TypingRace(40);
+    race.addTypist(new Typist('①', "TURBOFINGERS", 0.85), 1);
+    race.addTypist(new Typist('②', "QWERTY_QUEEN",  0.60), 2);
+    race.addTypist(new Typist('③', "HUNT_N_PECK",   0.30), 3);
+    race.startRace();
+}
+
     /**
      * Seats a typist at the given seat number (1, 2, or 3).
      *
